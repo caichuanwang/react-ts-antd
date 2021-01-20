77c06946809a0debbb5e7f533f862aaece71f3fc
caichuanwang <1481410897@qq.com>
2021-01-20 22:53:16 +0800

    开始表格

--- src/pages/course/index.tsx
+++ src/pages/course/index.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { Table } from 'antd';
 
-const index = () => {
-  return <div>这是学生课程</div>;
-};
+export interface IndexProps {}
 
-export default index;
+export interface IndexState {}
+
+class Index extends React.Component<IndexProps, IndexState> {
+  constructor(props: IndexProps) {
+    super(props);
+    this.state = { selectedRowKeys: [], list: [] };
+  }
+  render() {
+    const { selectedRowKeys } = this.state;
+    return (
+      <div>
+        <Table>
+          <Table.Column
+            align="center"
+            title="课程类别"
+            dataIndex="courseType"
+          ></Table.Column>
+          <Table.Column
+            align="center"
+            title="课程名称"
+            dataIndex="courseName"
+          ></Table.Column>
+          <Table.Column
+            align="center"
+            title="课程总价"
+            dataIndex="courseCount"
+          ></Table.Column>
+          <Table.Column
+            align="center"
+            title="课程数量"
+            dataIndex="courseNum"
+          ></Table.Column>
+          <Table.Column
+            align="center"
+            title="课程地址"
+            dataIndex="courseAddress"
+          ></Table.Column>
+          <Table.Column
+            title="操作"
+            key="action"
+            render={(text: string, record: any, index: number) => {
+              console.log(text, record, index);
+            }}
+          ></Table.Column>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export default Index;
